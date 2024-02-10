cfd96cbc69095595a2981a2836b781ccb7edc92f
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-02-10 22:54:32 +0530

    Refactoring the file storage
    This section we create to helper function and write the clean code

--- models/product.js
+++ models/product.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-const product = [];
+const products = [];
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+// Helper function to create we need to clean code
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -10,16 +26,7 @@ module.exports = class Product {
 
   // save the data
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -29,17 +36,6 @@ module.exports = class Product {
 
   // Fetch the data
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
