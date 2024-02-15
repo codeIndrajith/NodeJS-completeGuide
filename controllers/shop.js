c1f7bff20e262f0d6975cef8e0e6d7adb6d78f32
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-02-15 11:38:39 +0530

    Extracting the dynamic params
    we define a productID in identified unique items and get the id and id use to params.

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,6 +11,12 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productID;
+  console.log(prodId);
+  res.redirect('/');
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((product) => {
     res.render('shop/index', {
