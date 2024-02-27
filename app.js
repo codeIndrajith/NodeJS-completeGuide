bb554f8a305a9bd04b7b640434ab7b4112b1c41d
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-02-27 15:00:13 +0530

    Create mysql connection
    create a pool and set the connection.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const routes = require('./routes');
 const express = require('express');
 // const hbs = require('hbs');
 const path = require('path');
+const db = require('./util/database');
+const dotenv = require('dotenv');
 const app = express();
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const errorHandleController = require('./controllers/ErrorHandle');
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
 app.set('views', 'view');
+
+db.execute('DROP TABLE test');
 
 const adminRoutes = require('./routes/admin');
 const shopRouter = require('./routes/shop');
