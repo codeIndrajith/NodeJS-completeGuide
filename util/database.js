ce428cb3e91f9760c0ea423f975ec5a50aed92c0
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-03-02 16:11:23 +0530

    Connect the database using sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
+// // Manual connection in database
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'nodejsCourse',
+//   password: '',
+// });
+
+// module.exports = pool.promise();
+
+// using the sequelize to connect the database
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('nodejsCourse', 'root', '', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'nodejsCourse',
-  password: '',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
