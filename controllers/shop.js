c48268edda2005005f52f7f29d6edcc2b9cebead
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-03-29 15:45:29 +0530

    Creating & Fetching a Cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,28 +40,43 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart((cart) => {
-    Product.fetchAll((products) => {
-      const cartProducts = [];
-
-      for (product of products) {
-        const cartProductsData = cart.products.find(
-          (prod) => prod.id === product.id
-        );
-        if (cartProductsData) {
-          cartProducts.push({
-            productData: product,
-            qty: cartProductsData.qty,
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart
+        .getProducts()
+        .then((products) => {
+          res.render('shop/cart', {
+            path: '/cart',
+            PageTitle: 'Your Cart',
+            products: products,
           });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        PageTitle: 'Your Cart',
-        products: cartProducts,
-      });
-    });
-  });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+  // Cart.getCart((cart) => {
+  //   Product.fetchAll((products) => {
+  //     const cartProducts = [];
+
+  //     for (product of products) {
+  //       const cartProductsData = cart.products.find(
+  //         (prod) => prod.id === product.id
+  //       );
+  //       if (cartProductsData) {
+  //         cartProducts.push({
+  //           productData: product,
+  //           qty: cartProductsData.qty,
+  //         });
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       PageTitle: 'Your Cart',
+  //       products: cartProducts,
+  //     });
+  //   });
+  // });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
